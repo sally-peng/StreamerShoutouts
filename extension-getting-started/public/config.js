0e9517f089744267028c7af1429024c4cae0a9a4
sally-peng <ypeng813@gmail.com>
2019-09-14 15:45:03 -0400

    html and css buttons

--- extension-getting-started/public/config.js
+++ extension-getting-started/public/config.js
@@ -10,3 +10,12 @@ twitch.onAuthorized((auth) => {
   token = auth.token;
   userId = auth.userId;
 });
+
+function toggle() {
+  var x = document.getElementById("shoutout");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
